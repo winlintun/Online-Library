ca4327d22e8295a7f6e883e4f4395fd7fd1b7948
iceface <32415733+winlintun@users.noreply.github.com>
2022-09-02 12:40:06 +0630

    Update models.py
    image upload_to attribute fix

--- onlinebooks/models.py
+++ onlinebooks/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
 	description = models.TextField()
 	slug_url = models.SlugField(max_length=200, null=True, blank=True, help_text="a-bite-of-python")
 	book_link = models.CharField(max_length=2083)
-	image = models.ImageField(default='../static/onlinebooks/img/default.png', upload_to="../static/onlinebooks/img/")
+	image = models.ImageField(default='../static/onlinebooks/img/default.png', upload_to="media")
 	create_date = models.DateTimeField(default=timezone.now)
 	upload_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
